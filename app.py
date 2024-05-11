294613e0570a5ce8401739a9cc78494c53e318a3
HyominChoi <hiyoman1017@gmail.com>
2024-05-11 16:32:12 +0900

    ,

--- app.py
+++ app.py
@@ -94,6 +94,7 @@ def getName():
 def getImage():
     print(request.get_json()['action']['params'])
     urls = request.get_json()['action']['params']
+    print(urls.keys())
     urls = urls["입력 이미지"]["secureUrls"]
     urls[0] = 'l'
     
