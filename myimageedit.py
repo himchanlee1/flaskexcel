210fb41c433ff72a84ddfae6a8cd148f9f716383
HyominChoi <hiyoman1017@gmail.com>
2024-05-11 15:38:39 +0900

    PIL로 대체

--- myimageedit.py
+++ myimageedit.py
@@ -1,14 +1,12 @@
-import cv2
+from PIL import Image, ImageDraw, ImageFont
 from datetime import datetime
-
-
 
 def imageEdit(bill, date, img_path='bill/invoice.png'):
     # 이미지를 로드합니다.
     image_path = r'{}'.format(img_path)  # 로드할 이미지의 경로 (invoice)
-    image = cv2.imread(image_path)
+    image = Image.open(image_path)
 
-    # 추가할 텍스트와 좌표
+    # 텍스트와 좌표 설정
     texts_and_positions = {
         "{}".format(bill): (200, 670),  # House Air Bill #의 좌표
         "{}".format(date): (250, 695),   # Expected Date of delivery의 좌표
@@ -16,23 +14,23 @@ def imageEdit(bill, date, img_path='bill/invoice.png'):
         "SC": (200, 770)            # Shippers Name & Title의 좌표
     }
 
-    # OpenCV에서 사용할 폰트 설정
-    font = cv2.FONT_HERSHEY_PLAIN
-    font_scale = 1  # 텍스트 크기
-    font_color = (0, 0, 0)  # 텍스트 색상 (BGR)
-    thickness = 1  # 텍스트 두께
+    # 사용할 폰트와 크기 설정
+    font_path = "arial.ttf"  # 사용할 폰트 파일의 경로
+    font_size = 14
+    font = ImageFont.truetype(font_path, font_size)
 
-    # 각 텍스트와 좌표에 대해 이미지에 텍스트를 추가합니다.
+    # 텍스트와 좌표에 텍스트를 이미지에 추가합니다.
+    draw = ImageDraw.Draw(image)
+    font_color = (0, 0, 0)  # 텍스트 색상 (RGB)
     for text, position in texts_and_positions.items():
-        cv2.putText(image, text, position, font, font_scale, font_color, thickness)
+        draw.text(position, text, font=font, fill=font_color)
 
     # 현재 날짜와 시간을 포함하는 고유한 파일명을 생성합니다.
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    # output_path = rf'C:\Python\bill\modified_invoice_{timestamp}.png'
-    output_path = rf'bill/modified_invoide_{timestamp}.png'
+    output_path = rf'bill/modified_invoice_{timestamp}.png'
 
     # 변경된 이미지를 저장합니다.
-    cv2.imwrite(output_path, image)
+    image.save(output_path)
     print('편집 및 저장 완료.')
 
     return output_path
