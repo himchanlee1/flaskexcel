ecbe54ce276a00bfe69fe38a479ecd1366c059fe
HyominChoi <hiyoman1017@gmail.com>
2024-05-13 02:53:41 +0900

    끝없는 오류수정

--- kocr_test.py
+++ kocr_test.py
@@ -17,14 +17,14 @@ def clovaOCR(image_file):
     img_response = requests.get(image_file)
     image = Image.open(BytesIO(img_response.content))
     saved_filename = str(random.randint(0, 99999))
-    image.save('bill/'+saved_filename, 'jpg')
+    image.save('bill/'+saved_filename, 'JPEG')
 
-    image_file = 'bill/'+saved_filename+'.jpg'
+    image_file = 'bill/'+saved_filename+'.jpeg'
 
     request_json = {
         'images': [
             {
-                'format': 'jpg',
+                'format': 'jpeg',
                 'name': 'demo'
             }
         ],
