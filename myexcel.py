2b8536101681486fc124d2563ec6e98a595cc5e6
HyominChoi <hiyoman1017@gmail.com>
2024-05-18 13:18:04 +0900

    excel 업데이트

--- myexcel.py
+++ myexcel.py
@@ -1,6 +1,7 @@
 from openpyxl import load_workbook 
 from datetime import datetime
 
+
 def format_date(date_input):
     try:
         return datetime.strptime(date_input, "%y.%m.%d").strftime("%Y-%m-%d")
@@ -15,6 +16,7 @@ def update_excel(file_path, waybill_number, shipment_weight, pickup_date, ready_
     shipment_weight_num = float(shipment_weight)
     temperature_status = 'F' if shipment_weight_num >= 4.1 else 'A' if shipment_weight_num == 1.0 else 'Unknown'
 
+    sheet['B5'] = datetime.date.today().strftime("%Y-%m-%d")
     sheet['D5'] = waybill_number
     sheet['E5'] = temperature_status
     sheet['K5'] = format_date(pickup_date)
